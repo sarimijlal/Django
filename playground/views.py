239305b98c3ad8829bc464e61e6065c8bde57a42
sarimijlal <sarimijlal@gmail.com>
2025-10-31 22:14:20 +0500

    contains all the latest changes

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 # Create your views here.
 def hello(request):
-    return HttpResponse('Hello World')
+    # return render(request, "hello.html", {'name': 'sarim'})
+    return HttpResponse("Hello, sarim!")
+
+def sqlquery(request):
+    queryset = Product.objects.all()
+
+    for query in queryset:
+        print(query)
+
+    return HttpResponse('done')
--- storefront/urls.py
+++ storefront/urls.py
@@ -22,3 +22,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("playground/", include('playground.urls'))
 ]
+  
